73ede2d0e081adf4f9a177f3dc4c896398b02c88
Alex@0123 <sanskrutidharme@outlook.com>
2023-05-18 14:29:34 +0530

    Function added

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 
 import Add from './component/add.js';
-import {Sub,Multi} from './component/sub.js';
+import {Sub,Multi,Div} from './component/sub.js';
 
-Add()
+Add() 
 Sub()
 Multi()
+Div()
+
 console.log("app js")
--- component/sub.js
+++ component/sub.js
@@ -7,5 +7,6 @@ export const Multi = () => {
     console.log('3*2 =',3*2)
 };
 
-
-
+export const Div = () => {
+    console.log('4/2 =', 4/2)
+};
