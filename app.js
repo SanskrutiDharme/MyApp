f33160c8d9d06826a26836d96593d535221170a4
Alex@0123 <sanskrutidharme@outlook.com>
2023-05-18 14:09:22 +0530

    1st commit

---
+++ app.js
@@ -0,0 +1,8 @@
+
+import Add from './component/add.js';
+import {Sub,Multi} from './component/sub.js';
+
+Add()
+Sub()
+Multi()
+console.log("app js")
---
+++ component/add.js
@@ -0,0 +1,5 @@
+const Add = () => {
+  console.log('2+2 =', 2+2);
+};
+
+export default Add;
