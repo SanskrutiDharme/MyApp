85eea00c6e3828b33e11464a48c969e8b1ff82be
Alex@0123 <sanskrutidharme@outlook.com>
2023-05-19 15:47:45 +0530

    modified multiplication

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 import Add from './component/add.js';
 import Sub from './component/sub.js';
 import Multi from './component/multi.js';
@@ -6,7 +5,7 @@ import Div from './component/div.js';
 
 Add(2,4) 
 Sub(19,5)
-Multi(23,4)
+Multi(4)
 Div(12,6)
 
 console.log("app js")
--- component/multi.js
+++ component/multi.js
@@ -1,7 +1,11 @@
-const Multi = (x,y) => {
-    const multiply =  x * y ;
-    console.log(x + ' * ' + y + ' = ' +  multiply)
-};
+const tableof2 = (x1) => {
 
+    let y;
+    for(y=1; y<11; y++){
+        var tableoftwo = x1 * y;        
+        console.log( x1 + ' * ' + y + ' = ' + tableoftwo);
+    }
+    return tableoftwo;
+}
 
-export default Multi;
+export default tableof2;
