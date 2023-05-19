51d403b27fe3d858aabdf833fd1300a8dea3bbc8
Alex@0123 <sanskrutidharme@outlook.com>
2023-05-19 15:49:51 +0530

    app modified

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 import Add from './component/add.js';
 import Sub from './component/sub.js';
-import Multi from './component/multi.js';
+import tableof2 from './component/multi.js';
 import Div from './component/div.js';
 
 Add(2,4) 
 Sub(19,5)
-Multi(4)
+tableof2(4)
 Div(12,6)
 
 console.log("app js")
